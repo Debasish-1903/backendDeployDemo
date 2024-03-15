8e73ab00cd07e64738a05ae954599ad23447bc4e
Debasish-1903 <das31debasish@gmail.com>
2024-03-16 00:52:38 +0530

    index.js updated

--- index.js
+++ index.js
@@ -14,6 +14,44 @@ const port =4000;
 
 // respond with "hello world" when a GET request is made to the homepage
 
+
+
+const gitHubData=
+{
+  "login": "Debasish-1903",
+  "id": 135197479,
+  "node_id": "U_kgDOCA7zJw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/135197479?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Debasish-1903",
+  "html_url": "https://github.com/Debasish-1903",
+  "followers_url": "https://api.github.com/users/Debasish-1903/followers",
+  "following_url": "https://api.github.com/users/Debasish-1903/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Debasish-1903/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Debasish-1903/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Debasish-1903/subscriptions",
+  "organizations_url": "https://api.github.com/users/Debasish-1903/orgs",
+  "repos_url": "https://api.github.com/users/Debasish-1903/repos",
+  "events_url": "https://api.github.com/users/Debasish-1903/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Debasish-1903/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Debasish Das",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 8,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2023-06-01T04:45:48Z",
+  "updated_at": "2024-03-07T13:52:05Z"
+}
+
 app.get('/', (req, res) => {
   res.send('hello world')
 
@@ -34,6 +72,13 @@ app.get('/youtube',(req,res)=>{
     res.send('<h2>chaiAurcode</h2>')
     
     })
+
+
+    app.get('/github',(req,res)=>{
+       
+      res.json(gitHubData)
+    })
+
 
 
 app.listen(process.env.PORT,()=>{
